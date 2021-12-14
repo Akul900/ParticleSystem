b13426d7554a5c2eeed8b64e4c3f2f6dd1dcd417
Akulin <akulinaa2002@gmail.com>
2021-12-14 13:27:48 +0800

    Добавил новый клас для изменения частиц

--- ParticleSystem/Form1.cs
+++ ParticleSystem/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace ParticleSystem
 {
+
     public partial class Form1 : Form
     {
         List<Particle> particles = new List<Particle>();
@@ -18,16 +19,7 @@ namespace ParticleSystem
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
 
-            // генерирую 500 частиц
-            for (var i = 0; i < 500; ++i)
-            {
-                var particle = new Particle();
-                // переношу частицы в центр изображения
-                particle.X = picDisplay.Image.Width / 2;
-                particle.Y = picDisplay.Image.Height / 2;
-                // добавляю список
-                particles.Add(particle);
-            }
+
         }
 
         private void UpdateState()
@@ -38,12 +30,14 @@ namespace ParticleSystem
                 if (particle.Life < 0)
                 {
                     particle.Life = 20 + Particle.rand.Next(100);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
                     // перемещаю частицу в центр
-                    particle.X = picDisplay.Image.Width / 2;
-                    particle.Y = picDisplay.Image.Height / 2;
-                    particle.Direction = Particle.rand.Next(360);
-                    particle.Speed = 1 + Particle.rand.Next(10);
-                    particle.Radius = 2 + Particle.rand.Next(10);
+                    /* particle.X = picDisplay.Image.Width / 2;
+                     particle.Y = picDisplay.Image.Height / 2;
+                     particle.Direction = Particle.rand.Next(360);
+                     particle.Speed = 1 + Particle.rand.Next(10);
+                     particle.Radius = 2 + Particle.rand.Next(10);*/
                 }
                 else
                 {
@@ -51,6 +45,25 @@ namespace ParticleSystem
                     var directionInRadians = particle.Direction / 180 * Math.PI;
                     particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
                     particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+
+            }
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500)
+                {
+                    // а у тут уже наш новый класс используем
+                    var particle = new ParticleColorful();
+                    // ну и цвета меняем
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break;
                 }
             }
         }
@@ -66,21 +79,26 @@ namespace ParticleSystem
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
-            UpdateState(); // каждый тик обновляем систему
+            UpdateState();
 
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
-                g.Clear(Color.White);
-                Render(g); // рендерим систему
+                g.Clear(Color.Black); // А ЕЩЕ ЧЕРНЫЙ ФОН СДЕЛАЮ
+                Render(g);
             }
 
             picDisplay.Invalidate();
 
         }
 
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+
         private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-
+            // в обработчике заносим положение мыши в переменные для хранения положения мыши
+            MousePositionX = e.X;
+            MousePositionY = e.Y;
         }
     }
 }
