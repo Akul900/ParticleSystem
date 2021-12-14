6b819a0aa8d6292472b7a6596850f748315c0a3b
Akulin <akulinaa2002@gmail.com>
2021-12-14 13:00:57 +0800

    Сделал вывод частицы

--- ParticleSystem/Form1.cs
+++ ParticleSystem/Form1.cs
@@ -12,9 +12,43 @@ namespace ParticleSystem
 {
     public partial class Form1 : Form
     {
+        List<Particle> particles = new List<Particle>();
         public Form1()
         {
             InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            // генерирую 500 частиц
+            for (var i = 0; i < 500; ++i)
+            {
+                var particle = new Particle();
+                // переношу частицы в центр изображения
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                // добавляю список
+                particles.Add(particle);
+            }
+        }
+      
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White);
+                foreach (var particle in particles)
+                {
+                    particle.Draw(g);
+                }
+            }
+
+            picDisplay.Invalidate();
+
+        }
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+
         }
     }
 }
