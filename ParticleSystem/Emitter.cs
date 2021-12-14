c8d7be189e6629b4dfea0ddb8e03f05beaaf6083
Akulin <akulinaa2002@gmail.com>
2021-12-14 13:40:23 +0800

    Добавил класс эмиттер и гравитацию к точкам

--- ParticleSystem/Emitter.cs
+++ ParticleSystem/Emitter.cs
@@ -3,10 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace ParticleSystem
 {
     internal class Emitter
     {
+        List<Particle> particles = new List<Particle>();
+        public List<Point> gravityPoints = new List<Point>();
+        public int MousePositionX;
+        public int MousePositionY;
+        public float GravitationX = 0;
+        public float GravitationY = 0;
+
+        public void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1;  // не трогаем
+                if (particle.Life < 0)
+                {
+                    // тоже не трогаем
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+
+
+
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+
+
+                    // это не трогаем
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                }
+                else
+                {
+                    foreach (var point in gravityPoints)
+                    {
+                        float gX = point.X - particle.X;
+                        float gY = point.Y - particle.Y;
+                        float r2 = (float)Math.Max(100, gX * gX + gY * gY); // ограничил
+                        float M = 100;
+
+                        particle.SpeedX += (gX) * M / r2;
+                        particle.SpeedY += (gY) * M / r2;
+                    }
+
+                    // это не трогаем
+                    particle.SpeedX += GravitationX;
+                    particle.SpeedY += GravitationY;
+
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+                }
+            }
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500)
+                {
+                    // а у тут уже наш новый класс используем
+                    var particle = new ParticleColorful();
+                    // ну и цвета меняем
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public void Render(Graphics g)
+        {
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+
+            foreach (var point in gravityPoints)
+            {
+                g.FillEllipse(
+                    new SolidBrush(Color.Red),
+                    point.X - 5,
+                    point.Y - 5,
+                    10,
+                    10
+                );
+            }
+        }
     }
 }
--- ParticleSystem/Form1.cs
+++ ParticleSystem/Form1.cs
@@ -14,91 +14,48 @@ namespace ParticleSystem
     public partial class Form1 : Form
     {
         List<Particle> particles = new List<Particle>();
+        Emitter emitter = new Emitter(); // добавили эмиттер
+
         public Form1()
         {
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
 
+            emitter.gravityPoints.Add(new Point(
+                picDisplay.Width / 2, picDisplay.Height / 2
+            ));
 
+            // добавил еще две        
+            emitter.gravityPoints.Add(new Point(
+              (int)(picDisplay.Width * 0.75), picDisplay.Height / 2
+           ));
+
+            emitter.gravityPoints.Add(new Point(
+               (int)(picDisplay.Width * 0.25), picDisplay.Height / 2
+           ));
         }
 
-        private void UpdateState()
-        {
-            foreach (var particle in particles)
-            {
-                particle.Life -= 1;
-                if (particle.Life < 0)
-                {
-                    particle.Life = 20 + Particle.rand.Next(100);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-                    // перемещаю частицу в центр
-                    /* particle.X = picDisplay.Image.Width / 2;
-                     particle.Y = picDisplay.Image.Height / 2;
-                     particle.Direction = Particle.rand.Next(360);
-                     particle.Speed = 1 + Particle.rand.Next(10);
-                     particle.Radius = 2 + Particle.rand.Next(10);*/
-                }
-                else
-                {
-                    // а это наш старый код
-                    var directionInRadians = particle.Direction / 180 * Math.PI;
-                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
-                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
-                }
 
-            }
-            for (var i = 0; i < 10; ++i)
-            {
-                if (particles.Count < 500)
-                {
-                    // а у тут уже наш новый класс используем
-                    var particle = new ParticleColorful();
-                    // ну и цвета меняем
-                    particle.FromColor = Color.Yellow;
-                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-                    particles.Add(particle);
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-
-        // функция рендеринга
-        private void Render(Graphics g)
-        {
-            // утащили сюда отрисовку частиц
-            foreach (var particle in particles)
-            {
-                particle.Draw(g);
-            }
-        }
         private void timer1_Tick(object sender, EventArgs e)
         {
-            UpdateState();
+            emitter.UpdateState(); // тут теперь обновляем эмиттер
 
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
-                g.Clear(Color.Black); // А ЕЩЕ ЧЕРНЫЙ ФОН СДЕЛАЮ
-                Render(g);
+                g.Clear(Color.Black);
+                emitter.Render(g); // а тут теперь рендерим через эмиттер
             }
 
             picDisplay.Invalidate();
 
         }
 
-        private int MousePositionX = 0;
-        private int MousePositionY = 0;
 
         private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
             // в обработчике заносим положение мыши в переменные для хранения положения мыши
-            MousePositionX = e.X;
-            MousePositionY = e.Y;
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
     }
 }
